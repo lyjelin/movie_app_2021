42cf4ba4e94a7f61bc87749be35a52875725b783
lyjelin <u3560393@connect.hku.hk>
2021-05-21 13:32:16 +0800

    #4.2 Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie.js";
+import "./App.css";
 
 class App extends React.Component{
 
@@ -21,10 +22,12 @@ class App extends React.Component{
   render(){
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading ? "Loading..." : movies.map( movie => {
+      <div class="container">
+        {isLoading ? <div class="loader">
+          <span class="loader_text">"Loading..."</span>
+      </div> : movies.map( movie => {
           return (
-            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.poster} />
+            <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
           )})}         
       </div>
     )
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie ({id, year, title, summary, poster }) {
-    return <h3>{title}</h3>
+
+function Movie ({year, title, summary, poster }) {
+    return <div class="movies_movie">
+        <img src={poster} alt={title} title={title} />
+        <div class="movie_data">
+            <h3 class="movie_title">{title}</h3>
+            <h5 class="movie_year">{year}</h5>
+            <p class="movie_summary">{summary}</p>
+        </div>
+    </div>
 }
 
 Movie.propTypes = {
